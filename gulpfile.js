a4cab9d2bc05f77ffabbb2c86d40bf3d2df52262
bipin <bipin@studiomatrix.com.np>
2020-05-28 00:16:35 +0545

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+const autoprefixerOptions = {
+   browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+}
+function style() {
+    return gulp.src('scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream());
+}
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./scss/*.scss', style);
+    gulp.watch('./*.html').on('change', browserSync.reload);
+    gulp.watch('./js/*.js').on('change', browserSync.reload);
+}
+
+var browserSync = require('browser-sync').create();
+
+exports.style = style;
+exports.watch = watch;
