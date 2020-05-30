232ab6333f982b90337ccf6f93fa5b8d0646331e
bipin <bipin@studiomatrix.com.np>
2020-05-30 13:26:17 +0545

    banner state fixed and prize threshold added

--- js/script.js
+++ js/script.js
@@ -12,10 +12,11 @@ const sudoPrizerList = getElem('data-prize-list').getElementsByTagName('li');
 const prizeList = Array.from(sudoPrizerList).reverse();
 const timerContainer = getElem('data-timer');
 const timeDisplay = document.getElementById('base-timer-label');
-const welcomeBanner = getElem('data-welcome-banner');
-const congratulationBanner = getElem('data-congratulation-banner');
-const loserBanner = getElem('data-loser-banner');
-const startButton = getElem('data-start-button');
+const banner = getElem('data-banner');
+const welcomeBanner = getElem('data-welcome');
+const congratulationBanner = getElem('data-congratulation');
+const loserBanner = getElem('data-loser');
+const bannerButton = getElem('data-banner-button');
 const TIME_LIMIT = 20;
 const FULL_DASH_ARRAY = 283;
 const WARNING_THRESHOLD = 10;
@@ -28,12 +29,28 @@ const COLOR_CODES = {
    threshold: WARNING_THRESHOLD
  }
 };
+const prizeThreshold = {
+  0: {
+    reached: false
+  },
+  4: {
+    reached: false
+  },
+  7: {
+    reached: false
+  },
+  10: {
+    reached: false
+  }
+}
+let currentBannerType;
 let remainingPathColor = COLOR_CODES.info.color;
 let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
-
-
+let gameWon = false;
+let gameStarted = false;
+let gameEnded = false;
 var answerBox = null;
 var answerList= null;
 var answerListElems = null;
@@ -49,9 +66,15 @@ var disableLifeLineButton =  {
 const answerOptions = ['A.', 'B.', 'C.', 'D.'];
 var currentQuestionAnswer;
 var currentLevel = 0;
-startButton.addEventListener('click', e => {
-  welcomeBanner.classList.add('remove');
-})
+
+bannerButton.addEventListener('click', e => {
+  changeBannerState(banner, false);
+  changeBannerState(currentBannerType, false);
+  if (currentBannerType !== welcomeBanner) {
+      initGame();
+  }
+});
+
 lifeLineList.addEventListener('click', e=> {
   let selectedLifeLine;
   let targetLifeLine =  e.target;
@@ -108,6 +131,37 @@ function fetchQuestion() {
   questionBank = questions;
   })
 }
+function initGame() {
+  resetGameStates();
+  resetBannerStates();
+  fetchQuestion();
+  gameStarted = true;
+}
+function resetGameStates() {
+  gameWon = false;
+  gameStarted = false;
+}
+function setBannerButtonStates() {
+  bannerButton.innerText = gameStarted ? 'Return': 'Start';
+  if  (gameStarted && !gameWon) {
+        bannerButton.classList.add('lost-button');
+        return;
+  }
+  bannerButton.classList.remove('lost-button');
+}
+function resetBannerStates() {
+      resetBanner();
+      showBanner(welcomeBanner);
+}
+function changeBannerState(bannerType, type) {
+  if (type) {
+    bannerType.classList.remove('remove');
+    return;
+  }
+  bannerType.classList.add('remove');
+}
+
+
 function renderQuestion(question) {
         const questionBox = document.createElement('div');
         questionBox.classList.add('question-box');
@@ -179,27 +233,25 @@ function renderAnswerBoxAndList(answers) {
   answerList.classList.add('answer-list');
   answerList.setAttribute('id', 'answer-list');
   answers.forEach((ansObj, i) => {
-    const answer = document.createElement('li');
-    const answerOpt = document.createElement('span');
-    answerOpt.innerText = answerOptions[i];
-    answer.innerText = ansObj['answerText'];
-    answer.dataset.id = i;
-    answer.addEventListener('click', e => {
+  const answer = document.createElement('li');
+  const answerOpt = document.createElement('span');
+  answerOpt.innerText = answerOptions[i];
+  answer.innerText = ansObj['answerText'];
+  answer.dataset.id = i;
+  answer.addEventListener('click', e => {
       const id = e.target.dataset.id;
       if (twoTimesTempAnswer) {
-        e.target.classList.add('temp-select');
-        twoTimesTempAnswer = false;
-        return;
+          e.target.classList.add('temp-select');
+          twoTimesTempAnswer = false;
+          return;
       }
-      checkAnswer(id);
+     checkAnswer(id);
   });
-
     answer.appendChild(answerOpt);
     answer.classList.add('hide');
     answerList.appendChild(answer);
   });
   answerListElems = answerList.getElementsByTagName('li');
-  console.log('ansE', answerListElems);
   answerBox.appendChild(answerList);
   questionContainer.appendChild(answerBox);
 }
@@ -221,16 +273,113 @@ function getElem(data) {
 }
 function checkAnswer(id) {
     const allAnswers = currentQuestionAndAnswers['answers'];
-    allAnswers.forEach((ansObj, i) => {
-      if (id == i) {
-        if (currentLevel <10) {
+    let ansCorrect = false;
+    for (let i = 0; i < allAnswers.length; i++) {
+      if (id == i)
+      {
+        const status = allAnswers[id]['status'];
+        ansCorrect = (status == 'true');
+      }
+      setAnswerStatus(allAnswers[i]['status'], i);
+    }
+    if (ansCorrect) {
+      if (currentLevel <= 9) {
+        if (currentLevel === 9) {
+            gameWin();
+            return;
+        }
           canRenderNextQuestion = true;
           currentLevel++;
-        }
+          setPrizeThreshold();
       }
-      setAnswerStatus(ansObj['status'], i);
-    });
+    }
+    if (!ansCorrect) {
+      gameLost();
+    }
 
+}
+function setPrizeThreshold(level) {
+  if (level == 0 || level == 4 || level == 7 || level == 10) {
+          prizeThreshold[level].reached = true;
+  }
+}
+function gameWin() {
+  gameWon = true;
+  canRenderNextQuestion = false;
+  setTimeout(() => {
+    showCongratulationBanner();
+  }, 4000) ;
+}
+function gameLost() {
+  gameWon = false;
+  setTimeout(() => {
+    showLostBanner();
+  }, 4000) ;
+}
+function showCongratulationBanner() {
+  setCongratulationMessage();
+  showBanner(congratulationBanner);
+}
+function showLostBanner() {
+  setLostMessage();
+  showBanner(loserBanner);
+}
+function setCongratulationMessage() {
+  const prize = getPrizeValue();
+  congratulationBanner.innerHTML = `
+              <div class="confetti-container confetti">
+              </div>
+              <div class="confetti-container confetti">
+              </div>Congratulations! <br> You have won ${prize}
+              <div class="confetti-container confetti">
+              </div>
+              <div class="confetti-container confetti">
+              </div>
+              <div class="confetti-container confetti">
+              </div>
+              <div class="confetti-container confetti">
+              </div>
+              <div class="confetti-container confetti">
+              </div>
+              <div class="confetti-container confetti">
+              </div>
+              <div class="confetti-container confetti">
+              </div>
+              <div class="confetti-container confetti">
+              </div>`;
+}
+function setLostMessage() {
+      const prize = getPrizeValue();
+      loserBanner.innerHTML = `Sorry! Wrong Answer <br> You win ${prize}`;
+}
+function getPrizeValue() {
+    let prizeVal;
+    let prizeIndexArray = [];
+    let prizeIndex;
+    for(const index in prizeThreshold) {
+      console.log('index', index);
+      if (index <= currentLevel) {
+          prizeIndexArray.push(index);
+      }
+    }
+    prizeIndex = Math.max(...prizeIndexArray);
+    prizeVal = prizeList[prizeIndex].innerText;
+    return `Rs ${prizeVal}`;
+}
+function showWelcomeBanner() {
+  showBanner(welcomeBanner);
+}
+function showBanner(bannerType) {
+  changeBannerState(banner, true);
+  changeBannerState(bannerType, true);
+  currentBannerType = bannerType;
+  setBannerButtonStates();
+}
+function resetBanner() {
+  changeBannerState(banner, false);
+  changeBannerState(welcomeBanner,false);
+  changeBannerState(congratulationBanner, false);
+  changeBannerState(loserBanner, false);
 }
 function setAnswerStatus(status, id) {
   if (status == 'true') {
@@ -252,6 +401,8 @@ function googleLogic() {
   startTimer();
 }
 function pollLogic() {
+  timerContainer.classList.remove('remove');
+  startTimer();
 }
 function timesTwoLogic() {
   twoTimesTempAnswer = true;
@@ -321,28 +472,4 @@ function onTImesUp() {
       timerContainer.classList.add('remove');
   }, 5000)
 }
-fetchQuestion();
-/**
-const questionBank = [
-  {
-   "bronze": [{
-     "question": "How many province does Nepal have?",
-     "answers": [{
-       "answerText": "4",
-       "status": false
-       },
-     {
-       "answerText": "5",
-       "status": false
-       },
-     {
-       "answerText": "6",
-       "status": false
-       }, {
-         "answerText": '7',
-         "status": true
-         }]
-     }]
-   }
-];
-**/
+initGame();
