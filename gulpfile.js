7de5529750dcd6f619559f179486b14f8655b303
Bipin Shrestha <bipin@studiomatrix.com.np>
2020-09-06 15:30:56 +0545

    fixing gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,29 @@ const gulpIf = require('gulp-if');
 const cssnano = require('gulp-cssnano');
 const imagemin = require('gulp-imagemin');
 const cache = require('gulp-cache');
+const babel = require('gulp-babel');
 const del = require('del');
 
 function userref() {
     return gulp.src('index.html')
         .pipe(useref())
-        .pipe(gulpIf('js/script.js', uglify()))
         .pipe(gulpIf('css/main.css', cssnano()))
         .pipe(gulp.dest('dist'))
+}
+function jsUglify() {
+    return gulp.src('js/script.js')
+        .pipe(babel({
+            presets: ['@babel/preset-env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
 }
 function images() {
     return gulp.src('src/images/**/*.+(png|jpg|gif|svg)')
         .pipe(cache(imagemin({
             interlaced: true
         })))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist/assets/image'))
 }
 
 function style() {
@@ -30,7 +38,7 @@ function style() {
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer())
         .pipe(gulp.dest('css'))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 }
 function cleanDist(done) {
@@ -55,4 +63,4 @@ var browserSync = require('browser-sync').create();
 
 exports.style = style;
 exports.watch = watch;
-exports.build = series(cleanDist, style, userref, images);
+exports.build = series(cleanDist, style, userref,jsUglify, images);
